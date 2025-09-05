123eb377ac72512a26468305d3a6bfc136c79a77
doudouabdoulaye <abdoulayedoudou99@gmail.com>
2025-09-06 00:04:55 +0100

    Mise à jour de la configuration pour le déploiement sur GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(),], })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), //  N'oubliez pas ce plugin pour que Vite comprenne le JSX !
+    tailwindcss()
+  ],
+  base: '/e-commerce/' //  C'est la ligne clé pour le déploiement sur GitHub Pages.
+});
